55c241a0489b8abf0c0e54a5b1be8f5f3e2a2470
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-03-05 21:32:08 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -4,10 +4,10 @@ token = os.environ.get('TOKEN','')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
-@Client.on_message(filters.private & filters.command(["about"]))
+@Client.on_message(filters.private & filters.command(["help"]))
 async def start(client,message):
 	botdata(int(botid))
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @Rename_658_bot\nBuy Subscription :- @GreyMatter_Bot\nSubscribe :- https://youtube.com/@GreyMattersYT\n\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"Total User:- {total_user()}\nBot :- @RoyalRenamebot\n\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
